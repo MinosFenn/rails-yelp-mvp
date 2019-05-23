b6d3260206335d1b353aea247a9211ca856c2f0b
MinosFenn <joachim.simon.45@gmail.com>
2019-05-23 12:26:35 +0200

    ex yelp 22/05

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,28 @@
+# Controling reviews
+class ReviewsController < ApplicationController
+
+  def new
+    @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    # we need `restaurant_id` to asssociate review with corresponding restaurant
+    if @review.save
+      redirect_to restaurant_path (@restaurant)
+    else
+      render "restaurants/show"
+    end
+  end
+
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+end
